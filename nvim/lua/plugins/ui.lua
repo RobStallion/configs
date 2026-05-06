b1e2d59ca2e9f6a8f91faf04dfeaa24e2d4772e4
Robert Francis <robertefrancis18@gmail.com>
2026-05-06 22:52:12 +0100

    feat(ui): add catppuccin, switch active theme to latte
    Disable tokyonight (keep config for toggle), add catppuccin with
    mocha/macchiato/frappe variants commented out. Latte active.

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -1,12 +1,27 @@
 return {
   {
     "folke/tokyonight.nvim",
+    enabled = false,
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme("tokyonight-moon")
+      -- vim.cmd.colorscheme("tokyonight-storm")
+      -- vim.cmd.colorscheme("tokyonight-night")
+      -- vim.cmd.colorscheme("tokyonight-day")
+    end
+  },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
     enabled = true,
     lazy = false,
     priority = 1000,
     config = function()
-      -- vim.cmd.colorscheme("tokyonight-night")
-      vim.cmd.colorscheme("tokyonight-day")
+      -- vim.cmd.colorscheme("catppuccin-mocha")
+      -- vim.cmd.colorscheme("catppuccin-macchiato")
+      -- vim.cmd.colorscheme("catppuccin-frappe")
+      vim.cmd.colorscheme("catppuccin-latte")
     end
   },
 }
