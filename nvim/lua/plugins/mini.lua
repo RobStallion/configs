e8a3f9fea30e8c31e15ba6d21939ab5d7ac4fff3
Robert Francis <robertefrancis18@gmail.com>
2026-05-13 11:32:00 +0100

    feat(nvim): customize mini.statusline layout

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -5,7 +5,27 @@ return {
   lazy = true,
   event = "BufReadPre",
   config = function()
-    require("mini.statusline").setup()
+    require("mini.statusline").setup({
+      content = {
+        active = function()
+          local mode, mode_hl = MiniStatusline.section_mode({ trunc_width = 120 })
+          local diagnostics   = MiniStatusline.section_diagnostics({ trunc_width = 75 })
+          local filename      = MiniStatusline.section_filename({ trunc_width = 140 })
+          local fileinfo      = MiniStatusline.section_fileinfo({ trunc_width = 120 })
+          local location      = MiniStatusline.section_location({ trunc_width = 75 })
+
+          return MiniStatusline.combine_groups({
+            { hl = mode_hl,                  strings = { mode } },
+            { hl = "MiniStatuslineDevinfo",  strings = { diagnostics } },
+            "%<",
+            { hl = "MiniStatuslineFilename", strings = { filename } },
+            "%=",
+            { hl = "MiniStatuslineFileinfo", strings = { fileinfo } },
+            { hl = mode_hl,                  strings = { location } },
+          })
+        end,
+      },
+    })
     require("mini.ai").setup()
     require("mini.surround").setup()
     -- require("mini.pairs").setup()
