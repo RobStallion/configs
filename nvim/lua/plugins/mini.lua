a5ae42042861c32addd403569c0bc90e07d70cbb
Robert Francis <robertefrancis18@gmail.com>
2026-05-21 10:36:15 +0100

    adds render-markdown

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -29,6 +29,7 @@ return {
     require("mini.ai").setup()
     require("mini.surround").setup()
     require("mini.pairs").setup()
+    require("mini.icons").setup()
     -- require("mini.operators").setup()
   end
 }
