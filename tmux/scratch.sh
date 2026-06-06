0822cf49211e500680cfb21b191958930172560c
Robert Francis <robertefrancis18@gmail.com>
2026-06-06 14:02:03 +0100

    tmux: update scratchpad path to new ~/scratch/todo.md

--- tmux/scratch.sh
+++ tmux/scratch.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # -A: attach to existing 'popup' session if it exists, otherwise create it
-tmux new-session -A -s popup nvim --cmd 'let g:fixed_colorscheme=1' -c 'colorscheme rose-pine-dawn' ~/scratch.md
+tmux new-session -A -s popup -c ~/scratch nvim --cmd 'let g:fixed_colorscheme=1' -c 'colorscheme rose-pine-dawn' todo.md
