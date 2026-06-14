7c667ce8a875de836129597279527f8dfaa6dff6
Robert Francis <robertefrancis18@gmail.com>
2026-06-14 18:24:41 +0100

    feat(nvim): add code/line yanking keymaps and treesitter parsers

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -66,6 +66,55 @@ vim.keymap.set('n', '<leader>yp', function()
   yank(vim.fn.expand('%:p'), 'path')
 end, { desc = 'Yank full file path' })
 
+vim.keymap.set('n', '<leader>yl', function()
+  local file = vim.fn.expand('%:.')
+  if file == '' then
+    vim.notify('No file name associated with buffer', vim.log.levels.WARN)
+    return
+  end
+  local line = vim.fn.line('.')
+  local result = string.format('%s:%d', file, line)
+  yank(result, 'file with line number')
+end, { desc = 'Yank filename with line number' })
+
+vim.keymap.set('v', '<leader>yl', function()
+  local file = vim.fn.expand('%:.')
+  if file == '' then
+    vim.notify('No file name associated with buffer', vim.log.levels.WARN)
+    return
+  end
+  local start_line = vim.fn.line('v')
+  local end_line = vim.fn.line('.')
+  local min_line = math.min(start_line, end_line)
+  local max_line = math.max(start_line, end_line)
+  local result
+  if min_line == max_line then
+    result = string.format('%s:%d', file, min_line)
+  else
+    result = string.format('%s:%d-%d', file, min_line, max_line)
+  end
+  yank(result, 'file with line number(s)')
+  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<Esc>', true, false, true), 'n', true)
+end, { desc = 'Yank filename with line numbers' })
+
+vim.keymap.set('v', '<leader>yc', function()
+  local file = vim.fn.expand('%:.')
+  if file == '' then
+    vim.notify('No file name associated with buffer', vim.log.levels.WARN)
+    return
+  end
+  local start_line = vim.fn.line('v')
+  local end_line = vim.fn.line('.')
+  local min_line = math.min(start_line, end_line)
+  local max_line = math.max(start_line, end_line)
+  local ft = vim.bo.filetype
+  local lines = vim.api.nvim_buf_get_lines(0, min_line - 1, max_line, false)
+  local content = table.concat(lines, '\n')
+  local result = string.format('### %s:%d-%d\n```%s\n%s\n```', file, min_line, max_line, ft, content)
+  yank(result, 'markdown code block')
+  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<Esc>', true, false, true), 'n', true)
+end, { desc = 'Yank code block with file/line info' })
+
 -- Go to file shortcuts (gs opens file under cursor in vertical split)
 vim.keymap.set('n', 'gs', '<Cmd>vertical wincmd f<CR>', { desc = 'Go to file in vertical split' })
 
